8ad09334dc0311312ec3fcf63e8cb4e07dccf383
Gideon Mutai <giddymutai13@gmail.com>
2024-03-23 15:45:05 +0300

    Adds arrow functions

---
+++ index.js
@@ -0,0 +1,8 @@
+// An arrow function expression to divide
+const divide = (a, b) => a/b
+
+// An arrow function to square
+const square = a => a ** 2
+
+// An arrow function that adds two numbers
+const add = (a, b) => a + b
